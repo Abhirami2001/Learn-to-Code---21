02b54990d86a77aa4b13d73b5ad21cd4c40486a5
Abhirami2001 <77285161+Abhirami2001@users.noreply.github.com>
2021-05-20 20:33:20 +0530

    Create Day9.c

---
+++ Day9.c
@@ -0,0 +1,24 @@
+C program to print the HCF of two numbers
+#include <stdio.h>
+int main()
+{
+
+int a,b,i,HCF=1;
+
+scanf("%d%d",&a,&b);
+ 
+for(i=1;i<=a&&i<=b;i++)
+
+if(a%i==0&&b%i==0)
+HCF=i;
+
+printf("%d",HCF );
+
+
+
+    
+    
+return 0;
+}
+    
+    
