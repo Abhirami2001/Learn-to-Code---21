13b1e43be1da82810bca4e1e5bd8a396c4abe663
Abhirami2001 <77285161+Abhirami2001@users.noreply.github.com>
2021-06-05 13:07:03 +0530

    Create Day11.c

---
+++ Day11.c
@@ -0,0 +1,26 @@
+Write a C program to read n values from the user and store it in the array. Reprint the values stored in the array and also print the value of second element in the array.
+
+#include <stdio.h>
+
+int main()
+{
+int arr[10],i,n;
+
+scanf("%d",&n);
+
+printf("Entered array:");
+
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+    
+for(i=0;i<n;i++)
+{
+printf(" %d",arr[i]);
+}
+ 
+printf("\n%d",arr[1]);
+    
+return 0;
+}
